17298caa46d86473fbfe2a9f41b9876842ab624a
Flinn <358530466@qq.com>
2020-10-07 19:43:43 +0800

    发现修改了homepage为./后依然无法访问gitpage，所以修改vue.config.js里的publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/'
+    ? '/vue-smtally-website/'
     : '/',
   lintOnSave: false,
   chainWebpack: config =>{
